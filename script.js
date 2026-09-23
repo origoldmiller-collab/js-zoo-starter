6ff1ec489d6197ed65556ae0b8eb1f99bc652ab9
origoldmiller-collab <origoldmiller@students.berkeley.net>
2026-09-23 16:56:08 +0000

    broken port (404)

--- script.js
+++ script.js
@@ -1,9 +1,41 @@
 //starter code
+//python -m http.server
+class Animal  {
+  constructor(name , favFood) {
+    this.name = name;
+    this.favoriteFood = favFood;
+  }
+  sleep() {
+    console.log(this.name + " sleeps for 8 hours")
+  }
+  eat (food){
+    console.log(this.name + " eats " + food);
+    if (food == this.favoriteFood){
+      console.log("YUM!!! " + this.name + " wants more " + food);
+    }
+  }
+}
+class Tiger extends Animal {
+  constructor(name) {
+    super (name, "meat")
+  }
+}
+class Bear  {
+  constructor(name) {
+    super (name , "fish")
+  }
+  sleep() {
+    console.log(this.name + " hibernates for 4 months")
+  }
+}
 
 function run() {
-    sleep("Tigger");
-  }
-  
-  function sleep(name) {
-    console.log(name + " sleeps for 8 hours")
-  }
+ var tigger = new Tiger("Tigger", "meat");
+tigger.eat("meat");
+tigger.eat("kibble");
+var pooh = new Bear("Pooh", "fish");
+pooh.eat("fish");
+pooh.eat("meat");
+
+}
+run();
